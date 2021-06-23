aaa3fd8df19bab666b85f078ba8266ed19058ea7
Gabriel Enrique Diaz <officialdge@gmail.com>
2021-06-23 16:50:58 -0300

    first commit

---
+++ ejer2_cadena.py
@@ -0,0 +1,18 @@
+a = 56
+b = 34.34
+
+print(type(a))
+print(type(b))
+#tipo de dato cadena
+cadena = 'Hola estimado Diaz'
+print(type(cadena))
+print(cadena)
+
+numero = 6
+print(numero)
+print(type(numero))
+numero = "Gabriel Diaz"
+print(numero)
+print(type(numero))
+
+
---
+++ hola_mundo.py
@@ -0,0 +1,10 @@
+print("Hola Diaz")
+
+a = 56
+b = 34
+c = a + b
+print(c)
+numero = 45.34
+print(numero)
+
+#Primer ejercicio en python
